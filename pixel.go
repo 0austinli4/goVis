0ae845b5f465c13e0dcad8665d1b2c70f86febef
Austin <0austinli4@gmail.com>
2023-12-12 15:55:31 -0500

    move forks

--- pixel.go
+++ pixel.go
@@ -19,6 +19,7 @@ import (
 	"golang.org/x/image/font/basicfont"
 )
 
+// struct holds philosopher, count of eating, left/right fork, position of sprite and text, and whether it's eating or not
 type Philosopher struct {
 	name      string // name of philosopher
 	count     int    // count number of times they eat
@@ -29,19 +30,29 @@ type Philosopher struct {
 	eating    bool
 }
 
+// struct fork holds it's owner, the matrix (pos + sprite), and pos it has
 type Fork struct {
 	owner string // name of philosopher
 	mat   pixel.Matrix
 	pos   pixel.Vec
 }
 
+// slice to store philosophers structs
 var philosophers []*Philosopher = make([]*Philosopher, 0)
+
+// map for philosopher name to it's index (for animation)
 var nameToIndex = make(map[string]int)
+
+// map for the latest 8 text segments (for animation)
 var textSeg []string = make([]string, 0)
+
+// table of locks on the forks for sharing of resources
 var table []sync.Mutex = make([]sync.Mutex, 5)
+
+// slice of the forks
 var forks []*Fork
 
-// load in png pictures
+// load in png picture (as sprite object)
 func loadPicture(path string) (pixel.Picture, error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -55,17 +66,20 @@ func loadPicture(path string) (pixel.Picture, error) {
 	return pixel.PictureDataFromImage(img), nil
 }
 
-// initialize philsopohers animation (5 gopghers in circle)
+// initialize philsopohers animation (5 gophers in circle)
 func initializePhilosophers(win *pixelgl.Window) {
+	// load picture
 	pic, err := loadPicture("hiking.png")
 	if err != nil {
 		panic(err)
 	}
+	// create sprite (object with picture)
 	sprite := pixel.NewSprite(pic, pic.Bounds())
 	spritePos := pixel.V(0, 0)
-	//textPos := pixel.V(0, 0)
+	// intiialize text object for animations
 	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
 
+	// table of philosophers
 	philosophers = []*Philosopher{
 		{"Brandon", 0, 0, 1, spritePos, spritePos, false},
 		{"Sol", 0, 1, 2, spritePos, spritePos, false},
@@ -73,13 +87,15 @@ func initializePhilosophers(win *pixelgl.Window) {
 		{"Sophie", 0, 3, 4, spritePos, spritePos, false},
 		{"David", 0, 4, 0, spritePos, spritePos, false},
 	}
-	// assign name to index for animation
+
+	// assign nameToIndex map
 	i := 0
 	for _, p := range philosophers {
 		nameToIndex[p.name] = i
 		i++
 	}
-	// declare initial circle
+
+	// declare initial circle center and radius
 	centerX := (win.Bounds().Center()).X
 	centerY := (win.Bounds().Center()).Y
 	radius := 300.0
@@ -87,6 +103,7 @@ func initializePhilosophers(win *pixelgl.Window) {
 	angleIncrement := (2 * math.Pi) / float64(numSprites)
 	initialAngle := math.Pi / 2 // Start from the top
 
+	// allocate gopher over the circle with offset of angleincrement
 	for i := 0; i < numSprites; i++ {
 		angle := initialAngle + float64(i)*angleIncrement
 		spritePos = pixel.V(
@@ -94,10 +111,13 @@ func initializePhilosophers(win *pixelgl.Window) {
 			centerY+radius*math.Sin(angle),
 		)
 		mat := pixel.IM
+		// for sizing
 		mat = mat.ScaledXY(spritePos, pixel.V(0.15, 0.15))
+
 		philosophers[i].spritePos = spritePos
 		sprite.Draw(win, mat)
 
+		// for sizing to align text on top of gopher
 		radiusText := 220.0
 		textPos := pixel.V(
 			centerX-80+radiusText*math.Cos(angle),
@@ -137,7 +157,7 @@ func initializeForks(win *pixelgl.Window) {
 		{"", mat, pixel.V(0, 0)},
 		{"", mat, pixel.V(0, 0)},
 	}
-
+	// same logic as philosophers
 	centerX := (win.Bounds().Center()).X
 	centerY := (win.Bounds().Center()).Y
 	radius := 300.0
@@ -167,11 +187,12 @@ func initializeForks(win *pixelgl.Window) {
 // draw one new frame of animation
 // updates: philsopohers, fork ownership, display of actions in text
 func drawNewFrame(win *pixelgl.Window) {
-
+	// intialize pictures
 	forkPic, _ := loadPicture("fork.png")
 	standing, _ := loadPicture("hiking.png")
 	eating, _ := loadPicture("gamer.png")
 
+	// batches for more efficient display
 	batchStand := pixel.NewBatch(&pixel.TrianglesData{}, standing)
 	batchStand.Clear()
 
@@ -190,10 +211,20 @@ func drawNewFrame(win *pixelgl.Window) {
 
 	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
 
-	// draw visual for forks
+	// draw visual for forks, based on owner
 	for i := 0; i < len(forks); i++ {
-		mat := forks[i].mat
-		spriteFork.Draw(batchFork, mat)
+		mat := pixel.IM
+		if forks[i].owner == "" {
+			mat = forks[i].mat
+			spriteFork.Draw(batchFork, mat)
+		} else {
+			name := forks[i].owner
+			index := philosophers[nameToIndex[name]]
+			// add a slight offset for 2 forks
+			pos := pixel.V(index.spritePos.X-float64(i)*7, index.spritePos.Y+float64(i)*7+20)
+			mat = mat.ScaledXY(pos, pixel.V(0.15, 0.15))
+			spriteFork.Draw(batchFork, mat)
+		}
 
 		basicTxt := text.New(forks[i].pos, basicAtlas)
 		basicTxt.Color = colornames.Blueviolet
@@ -252,6 +283,7 @@ func drawNewFrame(win *pixelgl.Window) {
 	for !win.JustPressed(pixelgl.MouseButtonLeft) {
 		win.Update()
 	}
+
 }
 
 // declare philosophers to be eating
@@ -292,6 +324,7 @@ func (p *Philosopher) Dine(win *pixelgl.Window) {
 
 		textSeg = append(textSeg, p.name+" is eating round:"+fmt.Sprint(p.count))
 		p.Eat(win)
+
 		updateEat(p)
 		drawNewFrame(win)
 
