5d5fa908c12b92124fa8ae08f215f44acd923568
Austin <0austinli4@gmail.com>
2023-12-15 14:29:55 -0500

    final push

--- diningVis/main.go
+++ diningVis/main.go
@@ -9,43 +9,7 @@ import (
 	"golang.org/x/image/colornames"
 )
 
-// optional function - you can use either runOnClick() or
-//	runConstantAnimation()
-func runOnClick() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	cfg := pixelgl.WindowConfig{
-		Title:  "Pixel Rocks!",
-		Bounds: pixel.R(0, 0, 1024, 768),
-		VSync:  true,
-	}
-
-	win, err := pixelgl.NewWindow(cfg)
-	if err != nil {
-		panic(err)
-	}
-	win.Clear(colornames.Aliceblue)
-
-	initializeForks(win)
-	initializePhilosophers(win)
-
-	for !win.JustPressed(pixelgl.MouseButtonLeft) {
-		win.Update()
-	}
-
-	win.Update()
-
-	for _, philosopher := range philosophers {
-		go func(p *Philosopher) {
-			p.Dine(win)
-		}(philosopher)
-	}
-
-	wg.Wait()
-
-}
-
-func runConstantAnimation() {
+func runAnimation() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	cfg := pixelgl.WindowConfig{
@@ -77,11 +41,10 @@ func runConstantAnimation() {
 		}
 		win.Update()
 	}
-
 	wg.Wait()
 
 }
 
 func main() {
-	pixelgl.Run(runConstantAnimation)
+	pixelgl.Run(runAnimation)
 }
